527ee53b2e2c9e1c297ab0b5ebb7d77a460172bf
rafael_alcindo <rafael_alcindo@hotmail.com>
2018-09-28 16:06:25 -0300

    categorias

--- src/components/telaInicial/lado_direita/categorias_inicio/categorias_inicio.js
+++ src/components/telaInicial/lado_direita/categorias_inicio/categorias_inicio.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import './categorias_inicio.css';
 import Adra from '../../../../assets/imagem_especialidade/categorias/adra.png';
+import Artes_Habilidades from '../../../../assets/imagem_especialidade/categorias/artes_e_habilidades_manuais.jpg';
+import Atividades_Agricolas from '../../../../assets/imagem_especialidade/categorias/atividades_agricolas.jpg';
+import Atividades_Missionarias from '../../../../assets/imagem_especialidade/categorias/atividades_missionarias.jpg';
+import Atividades_Profissionais from '../../../../assets/imagem_especialidade/categorias/atividades_profissionais.jpg';
+import Atividades_Recreativa from '../../../../assets/imagem_especialidade/categorias/atividades_recreativas.jpg';
+import Ciencia_Saude from '../../../../assets/imagem_especialidade/categorias/ciencia_saude.jpg';
+import Estudo_Natureza from '../../../../assets/imagem_especialidade/categorias/estudos_da_natureza.jpg';
+import Habilidade_Domestica from '../../../../assets/imagem_especialidade/categorias/habilidades_domesticas.jpg';
+import Mestrados from '../../../../assets/imagem_especialidade/categorias/mestrados.jpg';
 
 const CategoriaInicio = (props) => {
     return(
@@ -10,6 +19,7 @@ const CategoriaInicio = (props) => {
                 <h2 className="titulo_especi_alta" >Categorias</h2>
             </div>
             <div className="row toda_categoria_inicio" >
+
                 <div className="col-sm-2" >
                     <div className="ADRA_categoria" >
                         <img className="imagem_categoria" src={Adra}  alt="categorias"/>
@@ -18,6 +28,88 @@ const CategoriaInicio = (props) => {
                         <h4>Adra</h4>
                     </div>
                 </div>
+
+                <div className="col-sm-2" >
+                    <div className="ADRA_categoria" >
+                        <img className="imagem_categoria" src={Artes_Habilidades}  alt="categorias"/>
+                    </div>
+                    <div className="texto_categoria" >
+                        <h4>Artes e Habilidades Manuais</h4>
+                    </div>
+                </div>
+
+                <div className="col-sm-2" >
+                    <div className="ADRA_categoria" >
+                        <img className="imagem_categoria" src={Atividades_Agricolas}  alt="categorias"/>
+                    </div>
+                    <div className="texto_categoria" >
+                        <h4>Atividades Agrícolas</h4>
+                    </div>
+                </div>
+
+                <div className="col-sm-2" >
+                    <div className="ADRA_categoria" >
+                        <img className="imagem_categoria" src={Atividades_Missionarias}  alt="categorias"/>
+                    </div>
+                    <div className="texto_categoria" >
+                        <h4>Atividades Missionárias</h4>
+                    </div>
+                </div>
+
+                <div className="col-sm-2" >
+                    <div className="ADRA_categoria" >
+                        <img className="imagem_categoria" src={Atividades_Profissionais}  alt="categorias"/>
+                    </div>
+                    <div className="texto_categoria" >
+                        <h4>Atividades Profissionais</h4>
+                    </div>
+                </div>
+
+                <div className="col-sm-2" >
+                    <div className="ADRA_categoria" >
+                        <img className="imagem_categoria" src={Atividades_Recreativa}  alt="categorias"/>
+                    </div>
+                    <div className="texto_categoria" >
+                        <h4>Atividades Recreativas</h4>
+                    </div>
+                </div>
+
+                <div className="col-sm-2" >
+                    <div className="ADRA_categoria" >
+                        <img className="imagem_categoria" src={Ciencia_Saude}  alt="categorias"/>
+                    </div>
+                    <div className="texto_categoria" >
+                        <h4>Ciência e Saúde</h4>
+                    </div>
+                </div>
+
+                <div className="col-sm-2" >
+                    <div className="ADRA_categoria" >
+                        <img className="imagem_categoria" src={Estudo_Natureza}  alt="categorias"/>
+                    </div>
+                    <div className="texto_categoria" >
+                        <h4>Estudos da Natureza</h4>
+                    </div>
+                </div>
+
+                <div className="col-sm-2" >
+                    <div className="ADRA_categoria" >
+                        <img className="imagem_categoria" src={Habilidade_Domestica}  alt="categorias"/>
+                    </div>
+                    <div className="texto_categoria" >
+                        <h4>Habilidade Domésticas</h4>
+                    </div>
+                </div>
+
+                <div className="col-sm-2" >
+                    <div className="ADRA_categoria" >
+                        <img className="imagem_categoria" src={Mestrados}  alt="categorias"/>
+                    </div>
+                    <div className="texto_categoria" >
+                        <h4>Mestrados</h4>
+                    </div>
+                </div>
+
             </div>
         </div>
     );
